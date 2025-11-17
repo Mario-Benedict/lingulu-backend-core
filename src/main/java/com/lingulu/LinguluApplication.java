1be15d0e9c5f8f24f1358542402292675ab9843d
Mario-Benedict <mariobenedict77@gmail.com>
2025-11-17 09:54:24 +0700

    chore: scaffold new Spring Boot project

---
+++ src/main/java/com/lingulu/LinguluApplication.java
@@ -0,0 +1,13 @@
+package com.lingulu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinguluApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinguluApplication.class, args);
+	}
+
+}
