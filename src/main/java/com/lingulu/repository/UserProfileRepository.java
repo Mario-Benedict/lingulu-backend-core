b82384daff825a335b267c7100c6cebc84b4db03
Diazagil <diazagil061@gmail.com>
2025-12-21 17:13:44 +0700

    unimportant commit

--- src/main/java/com/lingulu/repository/UserProfileRepository.java
+++ src/main/java/com/lingulu/repository/UserProfileRepository.java
@@ -3,7 +3,7 @@ package com.lingulu.repository;
 import com.lingulu.entity.UserProfile;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserProfileRepository extends JpaRepository<UserProfile, String>{
+public interface UserProfileRepository extends JpaRepository<UserProfile, String> {
 
     boolean existsByUsername(String username);
 }
