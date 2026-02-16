270dca333538918c2bc1c249f28326e6417643b3
Diazagil <diazagil061@gmail.com>
2026-02-17 01:11:23 +0700

    refactor: SectionContentController endpoint

--- src/main/java/com/lingulu/controller/SectionContentController.java
+++ src/main/java/com/lingulu/controller/SectionContentController.java
@@ -15,7 +15,7 @@ import java.util.List;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/sections")
+@RequestMapping("/api/sections")
 @RequiredArgsConstructor
 public class SectionContentController {
 
