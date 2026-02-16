9bbf69f624d48c6c5ca5d18900ae120078ae686d
Diazagil <diazagil061@gmail.com>
2026-02-16 23:23:36 +0700

    refactor: Change endpoint in LearningProgressController

--- src/main/java/com/lingulu/controller/LearningProgressController.java
+++ src/main/java/com/lingulu/controller/LearningProgressController.java
@@ -20,7 +20,7 @@ import java.util.UUID;
 
 
 @RestController
-@RequestMapping("/learning/progress")
+@RequestMapping("/api/learning/progress")
 @RequiredArgsConstructor
 public class LearningProgressController {
 
