8f5177f00f05ff4c1ab58b92b3c107633db8d59a
Mario-Benedict <mariobenedict77@gmail.com>
2026-02-18 16:01:24 +0700

    fix: ci pipeline

--- src/test/java/com/lingulu/LinguluApplicationTests.java
+++ src/test/java/com/lingulu/LinguluApplicationTests.java
@@ -2,8 +2,10 @@ package com.lingulu;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class LinguluApplicationTests {
 
 	@Test
